ab05bdf8f986c71339f27a3d03dba57206f4b14a
navneetgoel <goel.navneet4@gmail.com>
2020-12-29 12:25:06 -0800

    Added PyCrawler Class

--- webCrawler.py
+++ webCrawler.py
@@ -1,2 +1,14 @@
 import requests
 import re
+
+class PyCrawler(object):
+    def __init__(self, starting_url):
+        self.starting_url = starting_url
+        self.visited = set()
+    
+    def start():
+        pass
+
+if __name__ == "__main__":
+    crawler = PyCrawler()
+    crawler.start()
