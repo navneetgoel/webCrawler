98b071baa38f91b29d7b10c31d0f5e20ed0344f9
navneetgoel <goel.navneet4@gmail.com>
2020-12-29 12:29:02 -0800

    added get html method

--- webCrawler.py
+++ webCrawler.py
@@ -1,10 +1,20 @@
 import requests
 import re
+from urllib.parse import urlparse
 
 class PyCrawler(object):
     def __init__(self, starting_url):
         self.starting_url = starting_url
         self.visited = set()
+    
+    def get_html(self, url):
+        try:
+            html = requests.get(url)
+        except Exception as e:
+            print(e)
+            return ""
+        return html.content.decode('latin-1')
+
     
     def start():
         pass
